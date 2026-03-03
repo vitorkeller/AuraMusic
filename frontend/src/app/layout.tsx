c79f7959e439c3efa1408d9db6337de17f37481b
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-03-03 19:45:12 -0300

    feat: criação da arquitetura de pastas

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Aura Music",
+  description: "Streaming de música",
+  icons: {
+    icon: [
+      {
+        url: "/img/favicon_black.svg",
+        media: "(prefers-color-scheme: light)",
+      },
+      {
+        url: "/img/favicon_white.svg",
+        media: "(prefers-color-scheme: dark)",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
