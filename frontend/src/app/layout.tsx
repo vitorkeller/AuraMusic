2048bdb747a7b60c9836810242c9a58033e8d269
Lucas Moraes <lucas.camilo.m@gmail.com>
2026-03-24 14:33:52 -0300

    feat: criação da página de login

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+
+const geistSans = Quicksand({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Quicksand({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
